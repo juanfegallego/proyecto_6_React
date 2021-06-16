dc9f95b471d1c282446d41c89da04ef29d1be394
juanfegallego <juanfegallegodj@gmail.com>
2021-06-16 12:09:11 +0200

    added global css + card profile + moment

--- src/containers/Profile/Profile.jsx
+++ src/containers/Profile/Profile.jsx
@@ -1,5 +1,6 @@
-import React, {useState, useEffect} from 'react';
-import {useHistory} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import user from "../../components/img/user.png"
 
 import "./Profile.css";
 
@@ -8,12 +9,13 @@ const Profile = () => {
     let history = useHistory();
 
     const [userData, setUserData] = useState({
-        token: localStorage.getItem("token"), 
-        user: JSON.parse(localStorage.getItem("user"))});
+        token: localStorage.getItem("token"),
+        user: JSON.parse(localStorage.getItem("user"))
+    });
 
-    useEffect(()=> {
+    useEffect(() => {
         console.log(userData.token);
-    },[]);
+    }, []);
 
     const deslogea = () => {
         localStorage.clear();
@@ -21,25 +23,40 @@ const Profile = () => {
 
     }
 
-    if(userData.token){
+    if (userData.token) {
         return (
-            <div>
-                {userData.user.email}
-                <div className="botonLogOut" onClick={()=> deslogea()}>LOG OUT!!!</div>
+            <div className="bodyProfile">
+                <div className="cardProfile">
+                    <div className="profile"><img className="imgProfile" src={user} alt="" /></div>
+                    <div className="textProfile">
+                    <div>{userData.user.name} </div>
+                    <div>{userData.user.surname}</div>
+                    <div>{userData.user.email}</div>
+                    <div>{userData.user.phoneNumber}</div>
+                    <div>{userData.user.country}</div>
+                    <div>{userData.user.city}</div>
+                    <div>{userData.user.birthday}</div>
+                    </div>
+                    
+                </div>
+                <div className="botonLogOut" onClick={() => deslogea()}>LOG OUT</div>
             </div>
-        )
-    }else{
 
-        setTimeout(()=> {
+
+        )
+    } else {
+
+        setTimeout(() => {
             history.push("/login");
-        },3000)
+        }, 3000)
 
         return (
             <div>NO ESTAS LOGEADO, NO PUEDES ENTRAR AQUI</div>
         )
-    }}
+    }
+}
 
-    
+
 
 
 
