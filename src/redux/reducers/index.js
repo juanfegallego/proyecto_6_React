4f6f6be94de86dfe9bcf221e057f6ed02451ad62
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-06-16 19:19:51 +0200

    add redux to bookings

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import credentials from './credential-reducer';
+import characters from './reservas-reducer'
 
 const rootReducer = combineReducers({
     credentials,
+    characters
 });
 
 export default rootReducer;
