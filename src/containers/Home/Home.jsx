697d3cd2d0aef590a59a029be0f3f4d304684c9a
juanfegallego <juanfegallegodj@gmail.com>
2021-06-20 21:07:40 +0200

    restaurant menus added

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,21 +1,32 @@
 
 
+import { DatePicker } from 'antd';
 import React from 'react';
 import imageHome from "../../components/img/home.jpg";
 // import CarouselSlider from '../../components/Slider/Slider';
-
+import Restaurants from '../../components/Restaurants/Restaurants';
+import Maps from '../../components/Maps/Map';
 import "./Home.css"
 
 
 
 const Home = () => {
     return (
-        <div>
+        <div className="bodyHome">
             <div >
-                
+
                 <img className="imageHome" alt="Home" src={imageHome} />
                 {/* <CarouselSlider/> */}
+                <div className="textRestaurantes">RESTAURANTES</div>
+                <Restaurants />
+
+
+
+
+
+
             </div>
+
 
         </div>
     )
